5a57866dd11e2f0ce540a4bec4b5250e77ad2664
Luis Herrera Chamat <lherrerac@psl.com.co>
2021-01-14 11:18:49 -0500

    Separating test on different describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -2,47 +2,78 @@ import { browser } from 'protractor';
 // tslint:disable-next-line: max-line-length
 import { MenuContentPage, ProductListPage, ProductAddedModalPage, SummaryStepPage, SignInPage, AddressPage, ShippingPage, PaymentPage, BankPaymentPage, OrderSummaryPage } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const email = 'j.l-h@hotmail.com';
-  const password = 'vkvkvkvk';
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInPage: SignInPage = new SignInPage();
-  const addressPage: AddressPage = new AddressPage();
-  const shippingPage: ShippingPage = new ShippingPage();
-  const paymentPage: PaymentPage = new PaymentPage();
-  const bankpaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+describe('Testing T-shirt buying process', () => {
 
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-    await (browser.sleep(1000));
+  describe('opening web page', async () => {
+    beforeAll(async () => {
+      await browser.get('http://automationpractice.com/');
+      await (browser.sleep(500));
+    });
 
-    await menuContentPage.goToTShirtMenu();
-    await (browser.sleep(1000));
+    describe('Buying T-shirt', async() => {
+      beforeAll(async() => {
+        const menuContentPage: MenuContentPage = new MenuContentPage();
+        const productListPage: ProductListPage = new ProductListPage();
+        const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+        const summaryStepPage: SummaryStepPage = new SummaryStepPage();
 
-    await productListPage.goToShirt();
-    await (browser.sleep(1000));
+        await menuContentPage.goToTShirtMenu();
+        await (browser.sleep(1000));
 
-    await productAddedModalPage.goTocheckout();
+        await productListPage.goToShirt();
+        await (browser.sleep(1000));
 
-    await summaryStepPage.goToNextPage();
+        await productAddedModalPage.goTocheckout();
+        await (browser.sleep(500));
 
-    await signInPage.fillEmail(email);
-    await signInPage.fillPassword(password);
-    await signInPage.goToNextPage();
+        await summaryStepPage.goToNextPage();
+        await (browser.sleep(500));
+      });
 
-    await addressPage.goToNextPage();
+      describe('Signing in', async() => {
+        beforeAll(async() => {
+          const email = 'j.l-h@hotmail.com';
+          const password = 'vkvkvkvk';
+          const signInPage: SignInPage = new SignInPage();
+          await signInPage.fillEmail(email);
+          await signInPage.fillPassword(password);
+          await signInPage.goToNextPage();
+          await (browser.sleep(500));
+        });
 
-    await shippingPage.checkTermsOfService();
-    await shippingPage.goToNextPage();
+        describe('Select shipping address', async() => {
+          beforeAll(async() => {
+            const addressPage: AddressPage = new AddressPage();
+            const shippingPage: ShippingPage = new ShippingPage();
 
-    await paymentPage.goToBankPayment();
+            await addressPage.goToNextPage();
+            await (browser.sleep(500));
 
-    await bankpaymentPage.pay();
+            await shippingPage.checkTermsOfService();
+            await shippingPage.goToNextPage();
+            await (browser.sleep(500));
+          });
 
-    await (expect(orderSummaryPage.getMessage()).toBe('Your order on My Store is complete.'));
+          describe('Paying', async() => {
+            beforeAll(async() => {
+              const paymentPage: PaymentPage = new PaymentPage();
+              const bankpaymentPage: BankPaymentPage = new BankPaymentPage();
+
+              await paymentPage.goToBankPayment();
+              await (browser.sleep(500));
+
+              await bankpaymentPage.pay();
+              await (browser.sleep(500));
+            });
+
+            it('Buying process is successful', async () => {
+              const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+              await (expect(orderSummaryPage.getMessage())
+              .toBe('Your order on My Store is complete.'));
+            });
+          });
+        });
+      });
+    });
   });
 });
